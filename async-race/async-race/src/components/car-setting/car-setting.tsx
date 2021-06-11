34ebdb2e2a114257e7ee778ed188719dff72cccd
anstss <anastasiya31017@gmail.com>
2021-06-11 14:07:16 +0300

    feat: finish car-settings view

--- async-race/async-race/src/components/car-setting/car-setting.tsx
+++ async-race/async-race/src/components/car-setting/car-setting.tsx
@@ -8,7 +8,7 @@ const CarSetting = (props: CarSettingPropsInterface) => {
         <div className='car-setting form-group d-flex justify-content-center my-3'>
             <input type='text' className='form-control car-setting__input col-xs-2 mx-3' placeholder={placeholder}/>
             <input type='color' className='form-control car-setting__color col-xs-2'/>
-            <button className='btn btn-primary mx-3 btn-size'>{buttonText}</button>
+            <button className='btn btn-primary mx-3 btn-size-normal'>{buttonText}</button>
         </div>
     )
 }
--- async-race/async-race/src/components/car-settings/car-settings.tsx
+++ async-race/async-race/src/components/car-settings/car-settings.tsx
@@ -1,3 +1,4 @@
+import './car-settings.scss';
 import React from "react";
 import CarSetting from "../car-setting/car-setting";
 
@@ -6,6 +7,11 @@ const CarSettings = () => {
         <div className='mt-5'>
             <CarSetting placeholder='Enter car name' buttonText='Create new car'/>
             <CarSetting placeholder='Enter new car name' buttonText='Update car'/>
+            <div className='d-flex justify-content-center'>
+                <button className='btn btn-success mx-3 btn-size-sm'>Race</button>
+                <button className='btn btn-primary btn-size-sm'>Reset</button>
+                <button className='btn btn-info mx-3 btn-size-lg'>Generate cars</button>
+            </div>
         </div>
     )
 }
