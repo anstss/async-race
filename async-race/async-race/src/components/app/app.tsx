c69beaf8f9e95f1f0da1664b3a558a4a13fb9cac
anstss <anastasiya31017@gmail.com>
2021-06-11 17:35:04 +0300

    feat: add pagination-view

--- async-race/async-race/src/components/app/app.tsx
+++ async-race/async-race/src/components/app/app.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import Nav from "../nav/nav";
 import Garage from "../pages/garage/garage";
 import Winners from "../pages/winners/winners";
+import Pagination from "../pagination/pagination";
 
 const App = () => {
     return (
         <div className='mx-5'>
             <h1 className='app-title text-center my-2'>Async Race</h1>
             <Nav/>
-            {/*<Garage/>*/}
-            <Winners/>
+            <Garage/>
+            {/*<Winners/>*/}
+            <Pagination/>
         </div>
     )
 }
