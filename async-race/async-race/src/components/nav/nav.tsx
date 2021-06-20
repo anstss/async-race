4a81ff455bca0f720ab5c63974194041af43e6f7
anstss <anastasiya31017@gmail.com>
2021-06-20 21:44:54 +0300

    feat: fix winners-pagination

--- async-race/async-race/src/components/nav/nav.tsx
+++ async-race/async-race/src/components/nav/nav.tsx
@@ -5,10 +5,11 @@ import {bindActionCreators, Dispatch} from "redux";
 import * as actions from "../../actions";
 import {AsyncRaceApiServiceContext} from "../async-race-api-service-context/async-race-api-service-context";
 
-const Nav = ({showView, currentPage, cars, carsPositions, winners}: any) => {
+const Nav = ({showView, currentPage, cars, carsPositions, winners, setCurrentWinners, currentWinnersPage}: any) => {
 
   const asyncRaceApiService = useContext(AsyncRaceApiServiceContext);
   //TODO: refactor!!!
+  //TODO: process update winners to func
   return (
     <nav className='nav d-flex justify-content-center my-3'>
       <button className='btn btn-primary btn-lg mx-3'
@@ -34,7 +35,13 @@ const Nav = ({showView, currentPage, cars, carsPositions, winners}: any) => {
       <button className='btn btn-primary btn-lg mx-3'
               onClick={() => {
                 showView('winners');
-                asyncRaceApiService.updateWinners();
+                asyncRaceApiService.updateWinners()
+                  .then(() => {
+                    asyncRaceApiService.getCurrentWinners(currentWinnersPage)
+                      .then((winners) => {
+                        setCurrentWinners(winners);
+                      })
+                  });
               }
               }>
         Winners
@@ -44,10 +51,11 @@ const Nav = ({showView, currentPage, cars, carsPositions, winners}: any) => {
 }
 //TODO: uncomment it
 /*carsPositions*/
-const mapStateToProps = ({cars, currentPage}: StateInterface) => {
+const mapStateToProps = ({cars, currentPage, currentWinnersPage}: StateInterface) => {
   return {
     cars,
     currentPage,
+    currentWinnersPage
     // carsPositions //TODO: uncomment it
   }
 }
